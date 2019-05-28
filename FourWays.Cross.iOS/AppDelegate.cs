bc0d5f6d29c4e086c2f6504aad1db60a64708eda
Stanlyh <eduardostanly@outlook.com>
2019-05-27 22:34:50 -0500

    Test Concept Edu

---
+++ FourWays.Cross.iOS/AppDelegate.cs
@@ -0,0 +1,13 @@
+﻿namespace FourWays.Cross.iOS
+{
+    using Core;
+    using Foundation;
+    using MvvmCross.Platforms.Ios.Core;
+
+    [Register("AppDelegate")]
+    public class AppDelegate : MvxApplicationDelegate<MvxIosSetup<App>, App>
+    {
+    }
+}
+
+
